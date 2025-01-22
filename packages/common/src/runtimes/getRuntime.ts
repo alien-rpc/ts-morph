956c1af34abe1ad84f8f33d2ce5a691822611fab
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-01-22 13:04:42 -0500

    chore: fix build issue

--- packages/common/src/runtimes/getRuntime.ts
+++ packages/common/src/runtimes/getRuntime.ts
@@ -1,3 +1,6 @@
 import { NodeRuntime } from "./NodeRuntime";
+import type { Runtime } from "./Runtime";
 
-export const runtime = new NodeRuntime();
+export const runtime: Runtime = new NodeRuntime();
+
+export type { Runtime };
